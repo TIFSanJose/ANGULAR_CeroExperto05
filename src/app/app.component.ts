1bddae2628de64d2f6be59b5e7ff7682b8f0e536
ramon <ramon@ramon>
2021-10-04 14:21:43 -0300

    commit inicial, v81 Pipes, pipes json. del v73 a v81

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipe';
+  nombre  : string   = 'Capitan America';
+  arreglo : number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI      : number   = Math.PI;
+  xman: Object = {
+    nombre    : 'Wolverine',
+    edad      : 500, 
+    mutacion  : 'garras', 
+    direccion : {
+      calle: 'Primera',
+      numero: 24
+    }
+  }
 }
