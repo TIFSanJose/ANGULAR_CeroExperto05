13061c446c011893ccb905f55a03d3ba750a363d
ramon <ramon@ramon>
2021-10-04 14:56:54 -0300

    v83 pipe date. Diversas tipos de salidas. Tiene una configuracion para el idioma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   nombre  : string   = 'Capitan America';
   arreglo : number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI      : number   = Math.PI;
+  fecha   : Date     = new Date();  
+
   xman: Object = {
     nombre    : 'Wolverine',
     edad      : 500, 
